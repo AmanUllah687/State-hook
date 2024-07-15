13d7ec8bade334d743f9a342e202eaa94bbe01e1
AmanUllah687 <Amanch687@gmail.com>
2024-07-15 12:16:18 +0500

    intial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import HookMouse from './Components/HookMouse';
+import MouseConatiner from './Components/MouseContainor';
+function App() {
+  return (
+    <div className="App">
+    <MouseConatiner />
+    </div>
+  );
+}
+export default App;
