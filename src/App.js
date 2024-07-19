17f116d2aba5a5c4b512323c31abd5ae931e8892
AmanUllah687 <Amanch687@gmail.com>
2024-07-19 19:01:29 +0500

    DataFethching using useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-import React, {useReducer} from 'react';
+import React from 'react';
 import './App.css';
-import ComponentA from './Components/ComponentA';
-import ComponentB from './Components/ComponentB';
-import ComponentC from './Components/ComponentC';
-export const CountCountext = React.createContext()
-
-const initailState = 0
-const reducer = (state, action)  => {
-    switch(action) {
-        case 'increment' :
-        return state + 1
-        case 'decrement' :
-            return state - 1
-            case 'reset' :
-                return initailState
-                default:
-                    return state
-
-    }
-
-}
+import DataFetchingOne from './Components/DataFetchingOne';
 
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initailState)
   return (
-    <CountCountext.Provider value={{countState: count, countDispatch: dispatch}}>
     <div className="App">
-      Count - {count}
-      <ComponentA />
-      <ComponentB />
-      <ComponentC />
-    </div>
-    </CountCountext.Provider>
+<DataFetchingOne />
+      </div>
   );
 }
 export default App;
